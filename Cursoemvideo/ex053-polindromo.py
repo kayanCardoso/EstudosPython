e43b75ca07dbed97874e53df6cef2883c3234ed2
Kayan Cardoso Soares <132955279+kayanCardoso@users.noreply.github.com>
2024-01-09 16:22:37 -0300

    Add files via upload

--- Cursoemvideo/ex053-polindromo.py
+++ Cursoemvideo/ex053-polindromo.py
@@ -1,10 +1,11 @@
 frase = str(input('Informe a frase :\n')).replace(' ','').lower()
-tam = int(len(frase))
-fr = []
-frv = []
-for i in range(0,tam):
-    print(frase[0:i])
-    fr = frase[:tam]
-for i in range(0,tam):
-    frv = frase[tam:i]
-print(fr,frv)
+tam = len(frase)
+inverso = ''
+print(frase)
+for i in range(tam -1 ,-1,-1):
+    inverso += frase[i]
+
+if inverso == frase:
+    print(f'{frase} é igual ao seu inverso {inverso}, por isso é um \033[31mPALÍNDROMO!!!\033[m')
+else:
+    print(f'{frase} não é igual ao seu inverso {inverso}, por isso não é um \033[31mPALÍNDROMO!!!\033[m')
